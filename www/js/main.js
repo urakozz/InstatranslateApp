539908e2048d5acb121f14c0f30a3f8e8c7d6609
urakozz <urakozz@gmail.com>
2015-09-09 22:31:09 +0200

    cordova updates + what the hell i did here

--- www/js/main.js
+++ www/js/main.js
@@ -7,12 +7,12 @@
         locale: lang,
         paths: {
             GS:'service/GlobalService',
-            Framework7:'../build/js/framework7',
-            lodash: '../build/js/lodash',
-            domReady: '../build/js/domReady',
-            reqwest: '../build/js/reqwest',
-            moment: "../build/js/moment-with-locales.min",
-            OAuth:'../build/js/oauth'
+            Framework7:'vendor/framework7',
+            lodash: 'vendor/lodash',
+            domReady: 'vendor/domReady',
+            reqwest: 'vendor/reqwest',
+            moment: "vendor/moment-with-locales.min",
+            OAuth:'vendor/oauth'
         },
         shim: {
             Framework7:{exports: 'Framework7'}
@@ -21,6 +21,7 @@
     define("main", ["app"], function(bootstrap){
         bootstrap.initGlobals();
         bootstrap.initDebugger();
+        dbg.debug("inited");
         bootstrap.initWelcomeScreen("welcomeButton");
         bootstrap.initPullToRefresh();
         bootstrap.enviromentalOnload(bootstrap.onDeviceReadyHandler);
